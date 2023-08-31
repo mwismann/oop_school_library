c3d3a83939f229e0f50a1ba6b260cc7fd097ee41
Mathías Wismann <mwbertolucci@gmail.com>
2023-08-30 20:55:43 -0400

    Implemented Decorator Pattern: - Added a class Nameable to lib/nameable.rb - Implemented a correct_name method in Nameable - Modified Person class to inherit from Nameable - Modified Person class to use correct_name method - Added a BaseDecorator class to lib/nameable.rb - In the BaseDecorator constructor, assigned a nameable object from params to an instance variable - In the BaseDecorator, implemented a correct_name method that calls the correct_name method of the nameable object - Added a CapitalizeDecorator class to lib/nameable.rb, which inherits from BaseDecorator - In the CapitalizeDecorator, implemented a correct_name method that capitalizes the output of @nameable.correct_name - Added a TrimmerDecorator class to lib/nameable.rb, which inherits from BaseDecorator - On the TrimmerDecorator, implemented a correct_name method that trims the output of @nameable.correct_name if it is longer than 10 characters

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,19 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -19,3 +26,11 @@ class Person
     age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+p capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
