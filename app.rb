394e86a21b5846bf8aab3039f70c76771c2e641b
Mathías Wismann <mwbertolucci@gmail.com>
2023-09-01 19:35:12 -0400

    Add a basic UI: - Created a `app.rb` file to handle the console app entry-point    - It contains the following methods:       - List all books.       -List all people.       - Create a person (teacher or student, not a plain Person).       - Create a book.       - Create a rental.       - List all rentals for a given person id - Created a `main.rb` file to handle the UI     - It contains defines a method as an entry point for the app called main        - Present the user with a list of options to perform.        - Lets users choose an options.        - If needed, asks for parameters for the option.        - Has a way to exit the program.

--- app.rb
+++ app.rb
@@ -1,29 +1,191 @@
-require './lib/person'
-require './lib/decorators/capitalize_deco'
-require './lib/decorators/trimmer_deco'
-require './lib/classroom'
-require './lib/student'
 require './lib/book'
+require './lib/person'
 require './lib/rental'
+require './lib/student'
+require './lib/teacher'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmed_person.correct_name
+class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+  end
 
-classroom_a = Classroom.new('A')
-classroom_b = Classroom.new('B')
+  # General methods
+  def check_empty_list(list, list_name)
+    return unless list.empty?
 
-student = Student.new(22, classroom_a, 'maximilianus')
-p classroom_a.students
-p student.classroom.label
-p student.add_classroom(classroom_b)
-p classroom_b.students.map(&:name)
+    puts "There's no #{list_name} stored yet"
+    display_menu
+  end
 
-book = Book.new('The Lord of the Rings', 'J. R. R. Tolkien')
-rental = Rental.new(Time.now, book, person)
-p rental.book.title
-book.add_rental(rental)
-p book.rentals.map(&:person).map(&:name)
+  # Menu methods
+  def display_menu
+    menu = {
+      1 => 'List all books',
+      2 => 'List all people',
+      3 => 'Create a person',
+      4 => 'Create a book',
+      5 => 'Create a rental',
+      6 => 'List all rentals for a given person id',
+      7 => 'Exit'
+    }
+
+    puts "\nSelect an option by entering a number:"
+
+    menu.each do |key, value|
+      puts "#{key} - #{value}"
+    end
+
+    validate_option
+  end
+
+  def validate_option
+    option = gets.chomp.to_i
+
+    menu_actions = {
+      1 => :list_all_books,
+      2 => :list_all_people,
+      3 => :create_person,
+      4 => :create_book,
+      5 => :create_rental,
+      6 => :list_rentals_by_person_id,
+      7 => :exit
+    }
+
+    action = menu_actions[option]
+
+    if action
+      send(action)
+    else
+      puts 'Select a valid option'
+      display_menu
+    end
+  end
+
+  # Person methods
+  def list_all_people
+    check_empty_list(@people, 'people')
+
+    @people.each do |person|
+      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+
+    display_menu
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp
+
+    print 'Name: '
+    name = gets.chomp
+
+    @people << Student.new(age, nil, name)
+    puts 'New person (Student) created successfully'
+
+    display_menu
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp
+
+    print 'Name: '
+    name = gets.chomp
+
+    print 'Specialization: '
+    specialization = gets.chomp
+
+    @people << Teacher.new(age, specialization, name)
+    puts 'New person (Teacher) created successfully'
+
+    display_menu
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    input = gets.chomp
+    case input
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Please enter a valid number (1 or 2)'
+
+      create_person
+    end
+  end
+
+  # Book methods
+  def list_all_books
+    check_empty_list(@books, 'books')
+
+    @books.each do |book|
+      puts "Title: #{book.title}, Author: #{book.author}"
+    end
+
+    display_menu
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+
+    @books << Book.new(title, author)
+    puts 'Book created successfully'
+
+    display_menu
+  end
+
+  # Rental methods
+  def create_rental
+    puts 'Select a book from the following list by number: '
+
+    check_empty_list(@books, 'books')
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+    end
+    book_index = gets.chomp.to_i
+
+    puts 'Select a person from the following list by number (Not ID): '
+
+    check_empty_list(@people, 'people')
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_index = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp
+
+    @rentals << Rental.new(date, @people[person_index], @books[book_index])
+    puts 'Rental created successfully'
+
+    display_menu
+  end
+
+  def list_rentals_by_person_id
+    print 'ID of the person: '
+    id = gets.chomp.to_i
+
+    rentals = @rentals.select do |rental|
+      rental.person.id.to_i == id
+    end
+
+    if rentals.empty?
+      puts 'This person has no rentals'
+    else
+      puts "Rentals for person with ID #{id}:"
+      rentals.each do |rental|
+        puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}"
+      end
+    end
+
+    display_menu
+  end
+end
